5bf41f22fc0d65f4dea7108efecfc2b7883f29cf
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-04-29 10:15:16 +0530

    Script for Enemy AI

---
+++ EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    GameObject target;
+
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GotoTarget();
+    }
+
+    private void GotoTarget() 
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
