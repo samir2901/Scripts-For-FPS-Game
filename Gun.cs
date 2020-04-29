4ad14c805f6b9785d34dde13bd1946bd29824edd
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-04-29 10:14:33 +0530

    Update

--- Gun.cs
+++ Gun.cs
@@ -6,16 +6,21 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+
     public Camera fpsCamera;
     public ParticleSystem muzzleFlash;
     public float impactForce = 70f;
     public GameObject impactEffect;
     public AudioSource firingSound;
+
+    private float nextTimeToFire = 0f;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
             //Debug.Log("Yo! I am firing..");
+            nextTimeToFire = Time.time + (1f / fireRate);
             Shoot();
             firingSound.Play();
         }
