0b7ac1b307741c0f883eef0acb89ec3548f9c215
samir2901 <thunder.cse2018@gmail.com>
2020-05-02 12:29:37 +0530

    New Scripts are Added

---
+++ GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject fpsPlayer, gun, fpsCamera;
+    public GameObject gameOverPanel, gameUIPanel;    
+    public GameObject[] spawners;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fpsPlayer.GetComponent<PlayerControl>().health <= 0)
+        {
+            gameUIPanel.SetActive(false);
+            gameOverPanel.SetActive(true);            
+            foreach (GameObject spawner in spawners)
+            {
+                spawner.SetActive(false);
+            }
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+            fpsPlayer.GetComponent<PlayerControl>().enabled = false;
+            fpsCamera.GetComponent<MouseLook>().enabled = false;
+            gun.GetComponent<Gun>().enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            Cursor.visible = false;
+            gameOverPanel.SetActive(false);            
+        }
+    }
+}
---
+++ GameOver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RestartMap()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+}
