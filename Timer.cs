cbc33d9fbc9818b4852eb552d24da583fc4fb3f6
samir2901 <thunder.cse2018@gmail.com>
2020-05-03 10:45:50 +0530

    New Scripts Added

---
+++ Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float startTime = 60f;
+    private float timer;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = (Mathf.Round(timer)).ToString();            
+        }else if(timer <= 0f)
+        {
+            Cursor.visible = true;
+            gameManager.Win();            
+        }
+    }
+}
---
+++ UIElements.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIElements : MonoBehaviour
+{
+    //Scripts for button related functions
+    public void RestartMap()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
