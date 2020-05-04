a8651a637a4ba680254f661de9f5d1f708f63d7a
samir2901 <thunder.cse2018@gmail.com>
2020-05-04 13:11:41 +0530

    Script for Options Menu Added

---
+++ OptionsMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mainAudioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i =0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " X " + resolutions[i].height);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volume)
+    {
+        mainAudioMixer.SetFloat("MainVolume", volume);
+        //Debug.Log(volume);
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+}
