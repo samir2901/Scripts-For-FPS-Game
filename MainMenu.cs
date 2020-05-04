052fe1ecf6e4eabf7f78e74c21d8ce5818e91a72
samir2901 <thunder.cse2018@gmail.com>
2020-05-04 10:46:35 +0530

    Script for Main Menu added

---
+++ MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject quitGameDialogBox, mainMenuPanel, mapSelectPanel;
+   public void playGame()
+    {
+        mapSelectPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void optionMenu()
+    {
+        Debug.Log("Load Options Panel");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showquitGameDialogBox()
+    {
+        quitGameDialogBox.SetActive(true);
+    }
+
+    public void showMainMenu()
+    {
+        quitGameDialogBox.SetActive(false);
+        mainMenuPanel.SetActive(true);        
+    }
+
+    public void backtoMainMenu()
+    {
+        mapSelectPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void loadMap1()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+}
